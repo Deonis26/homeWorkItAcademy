1612e32013908ae78de603387dbf6c0577de1af9
Denis <85250266+Deonis26@users.noreply.github.com>
2023-10-08 01:40:14 +0300

    решение задачи на оценку №3

--- src/src/m_jc1_143_23/tasksAssessment/task3/TwoDimensionalArray.java
+++ src/src/m_jc1_143_23/tasksAssessment/task3/TwoDimensionalArray.java
@@ -12,30 +12,47 @@ public class TwoDimensionalArray {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите размерность массива: ");
         int n = scanner.nextInt();
+        int summaMatrix = 0;
+        double arithmeticMean = 0;
+        int counter=0;
+
         int arr[][];
         arr = new int[n][n];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                if (i == j  || i==arr.length-1-j) {
+                if (i == j || i == arr.length - 1 - j) {
                     arr[i][j] = 0;
-                } else if (i < j && j < arr.length - 1-i) {
-                    arr[i][j] = 1;
-                } else if (j>i && i > arr.length - 1-j){
-                    arr[i][j]=3;
-                }
-                else if (i>j && j > arr.length - 1-i){
-                    arr[i][j]=5;
-                }
-                else {
-                    arr[i][j] =2;
+                } else if (i < j && j < arr.length - 1 - i) {
+                    arr[i][j] = (int) (Math.random() * 20);
+                } else if (j > i && i > arr.length - 1 - j) {
+                    arr[i][j] = (int) (Math.random() * -20);
+                } else if (i > j && j > arr.length - 1 - i) {
+                    arr[i][j] = (int) (Math.random() * 20);
+                } else {
+                    arr[i][j] = (int) (Math.random() * -20);
                 }
             }
         }
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 System.out.print(" " + arr[i][j] + " ");
+                summaMatrix = summaMatrix + arr[i][j];
             }
             System.out.println();
         }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (summaMatrix < arr[i][j]) {
+                    arithmeticMean =arithmeticMean+arr[i][j];
+                    counter++;
+                }
+            }
+
+        }
+        arithmeticMean=arithmeticMean/counter;
+        System.out.println();
+        System.out.println("Сумма всех элементов: " + summaMatrix);
+        System.out.println("Cреднее арифмитическое всех элементов больше " + summaMatrix + ": "+arithmeticMean);
+
     }
 }
