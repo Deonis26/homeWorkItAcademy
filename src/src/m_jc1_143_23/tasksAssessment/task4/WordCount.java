36363af1a245e8cb2c45ec50109ffbf5784b27f7
Denis <85250266+Deonis26@users.noreply.github.com>
2023-10-29 02:27:40 +0300

    Update task assessment: 4.

--- src/src/m_jc1_143_23/tasksAssessment/task4/WordCount.java
+++ src/src/m_jc1_143_23/tasksAssessment/task4/WordCount.java
@@ -7,24 +7,24 @@ public class WordCount {
         знаками препинания.
         Пробелы могут присутствовать так же в начале и конце текста. */
         // String text = "Hello world";
-        // String text = " Hello world, hello Java, hello   students . The WORLD is beautiful  beautiful!";
-        //String text = "Hello world hello Java hello students The WORLD is beautiful";
+        // String text = " Hello world, hello Java, hello   students . The                    WORLD is beautiful !";
         String text = "Listen to the news from today and read the text at the same time. " +
-               "Listen to the news from today without reading the text.";
+                "Listen to the news from today without reading the text.";
         int dublicateWordCounter = 0;
+        text = text.trim().replaceAll("[^a-zA-Zа-яА-Я0-9]+", " ");
         String arr[] = text.split(" ");
         for (int i = 0; i < arr.length; i++) {
             System.out.println(i + ". " + arr[i]);
-            for (int j = i+1; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i].equalsIgnoreCase(arr[j])) {
                     dublicateWordCounter++;
                     break;
                 }
             }
         }
-        int counterUniqueWord=arr.length-dublicateWordCounter;
+        int counterUniqueWord = arr.length - dublicateWordCounter;
         System.out.println();
         System.out.println("Number of unique words: " + counterUniqueWord);
-        System.out.println("Количество элементов в массиве: "+arr.length);
+        System.out.println("Количество элементов в массиве: " + arr.length);
     }
 }
