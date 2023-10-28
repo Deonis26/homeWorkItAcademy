de9a8f6021bd4d37287d4470b492bcab910844ca
Denis <85250266+Deonis26@users.noreply.github.com>
2023-10-29 01:04:56 +0300

    add task assessment: 4. Delete image

---
+++ src/src/m_jc1_143_23/tasksAssessment/task4/WordCount.java
@@ -0,0 +1,30 @@
+package m_jc1_143_23.tasksAssessment.task4;
+
+public class WordCount {
+    public static void main(String[] args) {
+        /*Дан текст. Подсчитайте уникальное количество слов в тексте.
+        Учитывайте, что слова могут разделяться несколькими пробелами,
+        знаками препинания.
+        Пробелы могут присутствовать так же в начале и конце текста. */
+        // String text = "Hello world";
+        // String text = " Hello world, hello Java, hello   students . The WORLD is beautiful  beautiful!";
+        //String text = "Hello world hello Java hello students The WORLD is beautiful";
+        String text = "Listen to the news from today and read the text at the same time. " +
+               "Listen to the news from today without reading the text.";
+        int dublicateWordCounter = 0;
+        String arr[] = text.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i + ". " + arr[i]);
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i].equalsIgnoreCase(arr[j])) {
+                    dublicateWordCounter++;
+                    break;
+                }
+            }
+        }
+        int counterUniqueWord=arr.length-dublicateWordCounter;
+        System.out.println();
+        System.out.println("Number of unique words: " + counterUniqueWord);
+        System.out.println("Количество элементов в массиве: "+arr.length);
+    }
+}
