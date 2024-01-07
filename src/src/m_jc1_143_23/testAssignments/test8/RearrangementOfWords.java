b977a9b2de36c4464b72d552b5a070dacb1a99a7
Denis <85250266+Deonis26@users.noreply.github.com>
2024-01-07 12:09:08 +0300

    Update task: 9.

--- src/src/m_jc1_143_23/testAssignments/test8/RearrangementOfWords.java
+++ src/src/m_jc1_143_23/testAssignments/test8/RearrangementOfWords.java
@@ -12,9 +12,10 @@ public class RearrangementOfWords {
         //String text = "Hello world";
         //String text = "In mathematics and computer science, an algorithm is a finite sequence of rigorous instructions, " +
         //      "typically used to solve a class of specific problems or to perform a computation.";
-        String text = "The world is mine!";
+        //String text = "The world is mine!";
+        String text = "abc, abcd , abcef";
         String bufferText = text;
-        text = text.replaceAll("\\p{Punct}", " $0"); //мне подсказал интернет как отделить все знаки препинания
+        // text = text.replaceAll("\\p{Punct}", " $0"); //мне подсказал интернет как отделить все знаки препинания
         String[] arr = text.split(" ");
         String minWord = arr[0];
         String maxWord = arr[0];
@@ -22,9 +23,9 @@ public class RearrangementOfWords {
         int posMax = 0;
         String buffer;
         for (int i = 0; i < arr.length; i++) {
-            if (minWord.length()>= arr[i].length()){
-               if(arr[i].equals(".") || arr[i].equals("!") || arr[i].equals(",")){
-                   continue;
+            if (minWord.length() >= arr[i].length()) {
+                if (arr[i].equals(".") || arr[i].equals("!") || arr[i].equals(",") || arr[i].equals(" ")) {
+                    continue;
                 }
                 minWord = arr[i];
                 posMin = i;
@@ -37,17 +38,15 @@ public class RearrangementOfWords {
         buffer = arr[posMin];
         arr[posMin] = arr[posMax];
         arr[posMax] = buffer;
-        System.out.println("Минимальный элемент: "+minWord + " - позиция: " + posMin);
-        System.out.println("Максимальный элемент: "+maxWord + " - позиция: " + posMax);
+        System.out.println("Минимальный элемент: " + minWord + " - позиция: " + posMin);
+        System.out.println("Максимальный элемент: " + maxWord + " - позиция: " + posMax);
         System.out.println();
-        String textNew="";
+        String textNew = "";
         for (int i = 0; i < arr.length; i++) {
-            textNew=textNew+arr[i]+" ";
+            textNew = textNew + arr[i] + " ";
         }
-        textNew=textNew.replaceAll("\\s+\\B", ""); //полтора часа блииииииииииииин
+        textNew = textNew.replaceAll("\\s+\\B", ""); //полтора часа блииииииииииииин
         System.out.println(textNew);
         System.out.println(bufferText);
-
-
     }
 }
