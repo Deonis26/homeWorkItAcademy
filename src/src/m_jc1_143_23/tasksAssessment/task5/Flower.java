827d40d0ba2db6bc388efa1cbff0c941730483ee
Denis <85250266+Deonis26@users.noreply.github.com>
2024-01-14 20:20:17 +0300

    Update task: task 5.

---
+++ src/src/m_jc1_143_23/tasksAssessment/task5/Flower.java
@@ -0,0 +1,15 @@
+package m_jc1_143_23.tasksAssessment.task5;
+
+public class Flower {
+    private String name;
+    private String color;
+    private int prise;
+    private int timeWhenFlowerWithers;
+
+    public Flower(String name, String color, int prise, int timeWhenFlowerWithers) {
+        this.name = name;
+        this.color = color;
+        this.prise = prise;
+        this.timeWhenFlowerWithers = timeWhenFlowerWithers;
+    }
+}
