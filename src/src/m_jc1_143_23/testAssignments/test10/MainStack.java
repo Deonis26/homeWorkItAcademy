931d2190298043e027feeb633046838d40b9bc74
Denis <85250266+Deonis26@users.noreply.github.com>
2024-06-20 14:44:00 +0300

    update test10

--- src/src/m_jc1_143_23/testAssignments/test10/MainStack.java
+++ src/src/m_jc1_143_23/testAssignments/test10/MainStack.java
@@ -4,18 +4,25 @@ package m_jc1_143_23.testAssignments.test10;
 //а так же с дополнительной функцией max() за O(1)
 public class MainStack {
     public static void main(String[] args) {
-        StackMax s1 = new StackMax();
-        s1.push(44);
-        s1.push(7);
-        s1.push(-10);
-        s1.push(15);
+        StackMax stack = new StackMax();
+
+        stack.push(190);
+        stack.push(44);
+        stack.push(7);
+        stack.push(191);
+        /*s1.push(15);
         s1.push(122);
         s1.push(-1);
+        s1.push(222);
         s1.push(2);
         s1.push(8);
         s1.push(144);
         s1.push(150);
-        System.out.println(s1);
-        System.out.println(s1.max());
+        s1.push(151);*/
+        System.out.println(stack);
+        stack.pop();
+        System.out.println(stack);
+        System.out.println(stack.max());
+        System.out.println(stack.pop());
     }
 }
--- src/src/m_jc1_143_23/testAssignments/test10/StackMax.java
+++ src/src/m_jc1_143_23/testAssignments/test10/StackMax.java
@@ -1,17 +1,37 @@
 package m_jc1_143_23.testAssignments.test10;
 
+import java.util.Scanner;
 import java.util.Stack;
 
-public class StackMax extends Stack {
-    public int maxValue=Integer.MIN_VALUE;
-    public void push(int E){
-        if (maxValue<E){
-            maxValue=E;
-        }
-        addElement(E);
-    }
-    public int max(){
-        return maxValue;
+public class StackMax{
+    private Stack<Integer> stack;
+    private Stack<Integer> maxValues;
+
+    public StackMax() {
+        stack = new Stack<>();
+        maxValues = new Stack<>();
     }
 
+    public void push(int E) {
+        stack.push(E);
+        if (maxValues.isEmpty() || E >= maxValues.peek()) {
+            maxValues.push(E);
+        }
+    }
+
+    public Integer pop() {
+        int value = stack.pop();
+        if (value == maxValues.peek()) {
+            maxValues.pop();
+        }
+        return value;
+    }
+    public int max() {
+        return maxValues.peek();
+    }
+
+    @Override
+    public String toString() {
+        return "Стек: " + stack ;
+    }
 }
