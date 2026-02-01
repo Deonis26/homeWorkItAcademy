7d132000a82628fee5dd76c3095b96c4f108318e
Ди <denis.22518@gmail.com>
2026-02-01 22:29:55 +0300

    creating a new menu and transferring part of the implementation to it

--- src/src/m_jc1_143_23/tagGame/Menu.java
+++ src/src/m_jc1_143_23/tagGame/Menu.java
@@ -1,12 +1,35 @@
 package m_jc1_143_23.tagGame;
 
+import java.util.Scanner;
+
 public class Menu {
-    public Menu() {
-        System.out.println("new game (n)");
+    //String menuSelection;
+    public Menu(TagGame tag) {
+                System.out.println("new game (n)");
         System.out.println("save game (s)");
         System.out.println("load game (l)");
         System.out.println("quit game (q)");
-
+        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        String menuSelection = scanner.next();
+        switch (menuSelection) {
+            case "n":
+                System.out.println(tag);
+                tag.win();
+                break;
+            case "s":
+                tag.saveGame();
+                tag.win();
+                break;
+            case "l":
+                tag.loadGame();
+                System.out.println(tag);
+                tag.win();
+                break;
+            case "q":
+                tag.quit();
+                break;
+        }
     }
 }
 
--- src/src/m_jc1_143_23/tagGame/TagGameMain.java
+++ src/src/m_jc1_143_23/tagGame/TagGameMain.java
@@ -1,38 +1,9 @@
 package m_jc1_143_23.tagGame;
 
-import javax.imageio.IIOException;
-import java.io.File;
-import java.io.IOException;
-import java.util.Random;
-import java.util.Scanner;
-
 public class TagGameMain {
     public static void main(String[] args) {
-        Menu menu = new Menu();
-        //System.out.println(menu);
-        Scanner scanner = new Scanner(System.in);
-        String menuSelection = scanner.next();
-        System.out.println();
         TagGame tag = new TagGame();
-        switch (menuSelection) {
-            case "n":
-                //TagGame tag = new TagGame();
-                System.out.println(tag);
-                tag.win();
-                break;
-            case "s":
-                tag.saveGame();
-                tag.win();
-                break;
-            case "l":
-                tag.loadGame();
-                System.out.println(tag);
-                tag.win();
-                break;
-            case "q":
-                tag.quit();
-                break;
-        }
+        Menu menu = new Menu(tag);
 
     }
 }
